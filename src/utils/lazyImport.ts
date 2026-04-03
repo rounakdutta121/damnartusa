e8ac6df758902debe2f6dc57b579b9a07256219d
rounakdutta121 <rou.nak153d@gmail.com>
2026-04-03 10:06:24 +0530

    Fix navbar issue - remove lazy loading
    - Revert to direct imports without lazy loading
    - Remove lazyImport utility that was causing issues
    - This ensures Header and all content render correctly

--- src/utils/lazyImport.ts
+++
@@ -1,9 +0,0 @@
-import React, { lazy } from 'react';
-
-export const lazyNamed = <T extends React.ComponentType<any>>(
-  importFn: () => Promise<{ [K in string]: T }>,
-  name: string
-) => lazy(async () => {
-  const module = await importFn();
-  return { default: module[name] };
-});
